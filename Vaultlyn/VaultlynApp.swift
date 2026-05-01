655b55f8f1ce52886f4bc09570e4a57bb1b87fbe
Sidharth Prabhu <cyberzypher@gmail.com>
2026-05-02 00:45:48 +0530

    Initial Commit

---
+++ Vaultlyn/VaultlynApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultlynApp.swift
+//  Vaultlyn
+//
+//  Created by Sidharth Prabhu on 2026-05-02.
+//
+
+import SwiftUI
+
+@main
+struct VaultlynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VaultlynTests/VaultlynTests.swift
@@ -0,0 +1,17 @@
+//
+//  VaultlynTests.swift
+//  VaultlynTests
+//
+//  Created by Sidharth Prabhu on 2026-05-02.
+//
+
+import Testing
+@testable import Vaultlyn
+
+struct VaultlynTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
